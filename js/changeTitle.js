44f190ed3cce01419225870a23d202eddfdc0f84
anaggranja <71924260+anaggranja@users.noreply.github.com>
2020-11-27 20:12:01 +0100

    Add files via upload

---
+++ js/changeTitle.js
@@ -0,0 +1,6 @@
+const changeTitle = () => {
+    window.onblur = () => { document.title = "come 🔙 to anaggranja." }
+    // window.onfocus = () => { document.title = "anaggranja. Portfolio " }
+  }
+  
+  changeTitle();
